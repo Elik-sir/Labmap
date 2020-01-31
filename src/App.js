ea0d3a64602789d01f04ff8e81928d380b39372d
Андрей Смирнов <andsmi97@yandex.ru>
2020-01-31 23:08:04 +0300

    commented useless buttons

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ export default class App extends React.PureComponent {
   render() {
     return (
       <div>
-        <button className="btn" onClick={() => this.zoomOnViewerCenter_1()}>
+        {/* <button className="btn" onClick={() => this.zoomOnViewerCenter_1()}>
           Zoom in
         </button>
         <button className="btn" onClick={() => this.fitSelection_1()}>
@@ -86,7 +86,7 @@ export default class App extends React.PureComponent {
 
         <strong>OR</strong>
         {/* keep attention in this way onZoom and onPan cb aren't called */}
-        <button className="btn" onClick={() => this.zoomOnViewerCenter_2()}>
+        {/* <button className="btn" onClick={() => this.zoomOnViewerCenter_2()}>
           Zoom in
         </button>
         <button className="btn" onClick={() => this.fitSelection_2()}>
@@ -94,9 +94,9 @@ export default class App extends React.PureComponent {
         </button>
         <button className="btn" onClick={() => this.fitToViewer_2()}>
           Fit
-        </button>
+        </button> */}
 
-        <hr />
+        {/* <hr /> */}
 
         <ReactSVGPanZoom
           width={1000}
